832c751e3d7467c178d8c0feaeb4fb22d7c65a87
kitsorfan <kitsorfan@protonmail.com>
2022-03-29 21:35:20 +0300

    add block and blockchain

--- block.py
+++ block.py
@@ -1,22 +1,69 @@
+# DISTRIBUTED SYSTEMS - NTUA ECE 2021 - 2022
+# Stylianos Kandylakis
+# Kitsos Orfanopoulos
+# Christos Tsoufis
 
-import blockchain
+# --------------Prerequisites-----------------
+# pip install -r requirements.txt
 
 
+# --------------Terminal scripts--------------
+#export FLASK_APP=api.py		for windows set FLASK_APP=api.py
+#export FLASK_ENV=development       !!FOR DEBUG MODE!!
+#flask run -h localhost -p 8765 --cert=adhoc
+#set FLASK_DEBUG=1
 
+# --------------Online Tutorials--------------
+# https://medium.com/@karthikeyan.ranasthala/build-a-jwt-based-authentication-rest-api-with-flask-and-mysql-5dc6d3d1cb82
+
+
+import datetime;
+from collections import OrderedDict
+from Crypto.Hash import SHA256
+import json
 
 class Block:
-	def __init__(self):
-		##set
+	# 🟩🟩🟩
+	# create a BLOCK struct
+	# -> index
+	# -> timestamp
+	# -> transactions list
+	# -> nonce
+	# -> current_hash
+	# -> previous_hash
+	def __init__(self, index = -1, previousHash = None):
+		self.index = index
+		self.previousHash = previousHash
+		self.timestamp = datetime.datetime.now().timestamp()
+		self.nonce = 0
+		self.listOfTransactions=[]
+		self.hash = None
 
-		#self.previousHash
-		#self.timestamp
-		#self.hash
-		#self.nonce
-		#self.listOfTransactions
-	
-	def myHash:
-		#calculate self.hash
+
+	# 🟩🟩🟩🟩
+	def listToSerialisable(self):
+		final = []
+		for trans in self.listOfTransactions:
+			final.append(trans.__dict__)
+		return final
 
 
-	def add_transaction(transaction transaction, blockchain blockchain):
-		#add a transaction to the block
+	# 🟩🟩🟩🟩🟩
+	def myHash(self):
+		# we add all the data of the block to an ordered dictionary
+		dictData = OrderedDict([('index',self.index),('prev',self.previousHash),('tmsp',self.timestamp), ('nonce',self.nonce),('transactions',self.listToSerialisable())])
+		temp = json.dumps(dictData) # create JSON (needed to hash)
+		return SHA256.new(temp.encode()).hexdigest()
+
+
+	# 🟩🟩🟩🟩🟩🟩
+	def printBlock(self):
+		print('\n__Block no:' + str(self.index) + '__')
+		print('Timestamp: \t' + str(self.timestamp))
+		print('Nonce: \t\t' + str(self.nonce))
+		print('Transactions: \t')
+		for t in self.listOfTransactions:
+			print('\t\tSender ID: ' + str(t.senderID) + ' \t\tReceiver ID: '+ str(t.receiverID) + ' \t\tAmount: '+ str(t.amount)+'nc')
+			print('\t\Hash: ' + str(t.id))
+		print('Current hash: \t\t' + str(self.hash))
+		print('Previous Hash: \t' + str(self.previousHash))
---
+++ blockchain.py
@@ -0,0 +1,56 @@
+# DISTRIBUTED SYSTEMS - NTUA ECE 2021 - 2022
+# Stylianos Kandylakis
+# Kitsos Orfanopoulos
+# Christos Tsoufis
+
+# Perhaps useless
+# import binascii
+# from collections import OrderedDict
+# import Crypto
+# import Crypto.Random
+# from Crypto.Hash import SHA
+# from Crypto.PublicKey import RSA
+# from Crypto.Signature import PKCS1_v1_5
+# import node
+# from flask import Flask, jsonify, request, render_template
+# import copy
+# import transaction
+# import requests
+
+import block
+
+class Blockchain:
+
+	# 🟩🟩🟩 
+	def __init__(self):
+		self.blockList = [] # blockchain, a list of blocks
+	
+
+	# 🟩🟩🟩🟩
+	def createBlockchain(self, initialTransactions):
+		# first we create the first block 
+		bootstrap = block.Block(index = 0, previousHash = 1)
+		# we add the initialTransactions to this block and we found its hash
+		bootstrap.listOfTransactions.append(initialTransactions)
+		bootstrap.hash = bootstrap.myHash()
+		# note that for bootstrap we don't have to check the block (its previous hash etc)
+		self.addBlock(bootstrap) #
+
+
+
+	# 🟩🟩🟩🟩🟩
+	# adds a block to the blockchain
+	def addBlock(self, newBlock):
+		self.blockList.append(newBlock)
+		print('Current blockchain lenght: \t' + str(len(self.blockList)))
+		print('The blocks are: \t' + str(len(self.blockList)))
+		self.printChain()
+
+
+	# 🟩🟩🟩🟩🟩🟩
+	# calls printBlock for every block in the blockchain
+	def printChain(self):
+		print("\n ------------------\n")
+		for block in self.blockList:
+			block.printBlock()
+
